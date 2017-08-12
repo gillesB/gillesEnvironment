03f1c427b5510a5914576dc687f05d62cb9eb4a6
gbaatz <baatzgilles@gmail.com>
2017-08-13 01:04:52 +0200

    Changed spaceship target path

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/usr/bin/zsh
 
 #Check the needed programs are installed
 reqs="Install zsh, vim, wget, git and tmux"
@@ -13,7 +13,8 @@ fi
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # Download spaceship theme
-wget https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh -O $ZSH_CUSTOM/themes/spaceship.zsh-theme
+echo $ZSH
+wget https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/spaceship.zsh -O $ZSH/custom/themes/spaceship.zsh-theme
 
 # Install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
